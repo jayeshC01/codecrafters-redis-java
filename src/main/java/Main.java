cc70c014904462e7d781fb5f627972d3820cb12c
Jayesh Chaudhari <noreply@email.com>
2025-08-27 12:16:19 +0530

    Task 3 with Threads

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,6 +3,7 @@ import java.net.ServerSocket;
 import java.io.OutputStream;
 import java.net.Socket;
 
+
 public class Main {
   public static void main(String[] args){
     // You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -13,29 +14,16 @@ public class Main {
         int port = 6379;
         try {
           serverSocket = new ServerSocket(port);
-          // Since the tester restarts your program quite often, setting SO_REUSEADDR
-          // ensures that we don't run into 'Address already in use' errors
           serverSocket.setReuseAddress(true);
-          // Wait for connection from client.
-          clientSocket = serverSocket.accept();
-          while (true) {
-            // Read input from client.
-            byte[] input = new byte[1024];
-            clientSocket.getInputStream().read(input);
-            String inputString = new String(input).trim();
-            System.out.println("Received: " + inputString);
-            clientSocket.getOutputStream().write("+PONG\r\n".getBytes());
+          System.out.println("Redis server active on port "+port);
+          while(true) {
+            // Wait for connection from client.
+            clientSocket = serverSocket.accept();
+            System.out.println("New client connected: " + clientSocket.getInetAddress());
+            new Thread(new ClientHandler(clientSocket)).start();
           }
         } catch (IOException e) {
           System.out.println("IOException: " + e.getMessage());
-        } finally {
-          try {
-            if (clientSocket != null) {
-              clientSocket.close();
-            }
-          } catch (IOException e) {
-            System.out.println("IOException: " + e.getMessage());
-          }
         }
     }
 }
