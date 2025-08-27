e7196b7529823fd56d311845f1d41bb93645f03e
Jayesh Chaudhari <noreply@email.com>
2025-08-27 14:03:07 +0530

    Implement Echo

--- src/main/java/ClientHandler.java
+++ src/main/java/ClientHandler.java
@@ -13,13 +13,19 @@ class ClientHandler implements Runnable  {
 
   public void run() {
     try {
-      while (true) {
+      BufferedReader reader =
+          new BufferedReader(
+              new InputStreamReader(clientSocket.getInputStream()));
+      BufferedWriter write =
+          new BufferedWriter(
+              new OutputStreamReader(clientSocket.getOutputStream()));
+      String cmd;
+      while ((cmd = reader.readLine()) != null) {
         // Read input from client.
-        byte[] input = new byte[1024];
-        clientSocket.getInputStream().read(input);
-        String inputString = new String(input).trim();
-        System.out.println("Received: " + inputString);
-        clientSocket.getOutputStream().write("+PONG\r\n".getBytes());
+        System.out.println("Received: " + cmd );
+        String response = processCommand(cmd);
+        writer.write(response);
+        write.flush();
       }
     } catch (IOException e) {
       System.out.println("IOException: " + e.getMessage());
@@ -31,6 +37,23 @@ class ClientHandler implements Runnable  {
       } catch (IOException e) {
         System.out.println("IOException: " + e.getMessage());
       }
+    }
+  }
+
+  public String processCommand(String cmd) {
+    cmd = cmd.toLowerCase().trim();
+    switch(cmd) {
+      case "ping":
+        return "PONG";
+      case "echo": {
+        String[] cmdparts = cmd.split(" ")
+        if(cmdparts.length !=2) {
+          return "-ERR invalid command ECHO"
+        }
+        return "$"+cmdparts[1].length+"\r\n"+cmdparts[1]+"\r\n";
+      }
+      default:
+        return "Invalid Command"
     }
   }
 }
